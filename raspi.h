69afca81b6afe6697c317764331d479fac6fee2b
jandro1111 <jandro1111@jandro1111-VirtualBox>
2021-01-11 19:49:14 -0300

    prueba

---
+++ raspi.h
@@ -0,0 +1,10 @@
+#ifndef RASPI_H
+#define RASPI_H
+#include <stdio.h>
+#include "termlib.h"
+#include "disdrv.h"
+#include "joydrv.h"
+#include "backend.h"
+void rasprint (int [][ANCHO]);//imprime en la pantalla de leds
+void raspinit(void);//inicializa librerias
+#endif
